8fe0e17abdaabb0780a083029bf31311f1ac967c
Ján Kľuka <jan.kluka@fmph.uniba.sk>
2023-11-06 18:28:28 +0000

    Allow copying of personal number

--- Aplikácia/include/sha1sums.php
+++ Aplikácia/include/sha1sums.php
@@ -1,4 +1,4 @@
 <?php $sha1sums = array(
 	"main.js" => "1f8ea5f2187f0fd985b1e359baf7a5572d484fe4",
-	"main.css" => "4ed19a169cca0cf4fc941f8fea3fea68b642d725",
+	"main.css" => "5893443af9506582147a7c37ab8c10fe43851afa",
 );
