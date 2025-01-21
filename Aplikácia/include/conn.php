8f8222099e382f5a9cf757b8e37bc52b425f732c
Cerulikova <cerulikova2@uniba.sk>
2025-01-21 18:38:18 +0100

    Update conn.php

--- Aplikácia/include/conn.php
+++ Aplikácia/include/conn.php
@@ -1,7 +1,7 @@
 <?php
 
 ini_set("mysqli.default_socket", "/tmp/mysql.sock");
-$conn = new mysqli("localhost", "root", "", "tis2");
+$conn = new mysqli("omnisqa.com", "pritomnost_new", "GuliverovaKluka", "pritomnost_new", "4321");
 if($conn->connect_error) die("Connection failed... DB - ERROR");
 $conn->set_charset("utf8");
 
